d9437b3a13ea4a967bb08cfc94b5ec0fd117a102
Noahlinton32 <noahlinton@gmail.com>
2023-07-27 21:19:02 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,70 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
+
+# Replace 'data.csv' with the actual filename and path of your CSV file
+csv_file = 'data.csv'
+
+# Read the CSV file into a pandas DataFrame
+df = pd.read_csv(csv_file)
+
+# Removing outliers from 'price' and 'house_size' columns
+df = df[
+    (df['price'] >= df['price'].mean() - 3 * df['price'].std()) &
+    (df['price'] <= df['price'].mean() + 3 * df['price'].std())
+]
+df = df[
+    (df['house_size'] >= df['house_size'].mean() - 3 * df['house_size'].std()) &
+    (df['house_size'] <= df['house_size'].mean() + 3 * df['house_size'].std())
+]
+
+# Calculate descriptive statistics
+statistics = df.describe()
+
+# Print the statistics with legends
+print("Descriptive Statistics:\n")
+print(statistics)
+
+# Additional Statistics
+print("\nAdditional Statistics:\n")
+print(f"Number of rows: {len(df):,}")
+print(f"Average price: ${df['price'].mean():,.2f}")
+print(f"Minimum price: ${df['price'].min():,.2f}")
+print(f"Maximum price: ${df['price'].max():,.2f}")
+print(f"Average number of bedrooms: {df['bed'].mean():.1f}")
+print(f"Average number of bathrooms: {df['bath'].mean():.1f}")
+
+# Visualizations
+# Histogram of prices
+plt.figure(figsize=(8, 6))
+plt.hist(df['price'], bins=20, edgecolor='black', color='skyblue')
+plt.xlabel('Price')
+plt.ylabel('Frequency')
+plt.title('Histogram of Prices')
+plt.gca().xaxis.set_major_formatter(mticker.StrMethodFormatter('${x:,.0f}'))
+plt.grid(True)
+plt.show()
+
+# Scatter plot of house size vs. price
+plt.figure(figsize=(8, 6))
+plt.scatter(df['house_size'], df['price'], color='orange', alpha=0.7)
+plt.xlabel('House Size')
+plt.ylabel('Price')
+plt.title('House Size vs. Price')
+plt.gca().xaxis.set_major_formatter(mticker.StrMethodFormatter('{x:,.0f}'))
+plt.gca().yaxis.set_major_formatter(mticker.StrMethodFormatter('${x:,.0f}'))
+plt.grid(True)
+plt.show()
+
+# Bar plot of the number of houses in each state (only showing states with at least 100 data points)
+state_counts = df['state'].value_counts()
+filtered_state_counts = state_counts[state_counts >= 100]
+plt.figure(figsize=(8, 6))
+filtered_state_counts.plot(kind='bar', color='green')
+plt.xlabel('State')
+plt.ylabel('Number of Houses')
+plt.title('Number of Houses in Each State')
+plt.gca().yaxis.set_major_formatter(mticker.StrMethodFormatter('{x:,.0f}'))
+plt.grid(axis='y')
+plt.show()
 
